8eac0a129f15c1057b2cdfb3c131c2356e207b4a
Simeï Steiner <simei.steiner@m-f.ch>
2023-08-21 19:25:53 +0200

    Step 5: Add navigation

--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -1,11 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from 'src/app/services/hero.service';
 import { Hero } from '../../interfaces/hero';
+
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent {
-  @Input() hero?: Hero;
+export class HeroDetailComponent implements OnInit{
+  hero: Hero | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private heroService: HeroService
+  ) { }
+
+  ngOnInit(): void{
+    this.getHero();
+  }
+
+  getHero(): void{
+    const id: number = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+        .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
